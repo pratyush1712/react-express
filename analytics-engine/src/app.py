41736d3c5727c94fedd499fadda59da7349f228e
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-04-04 06:13:41 -0400

    setup engine

---
+++ analytics-engine/src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import pandas as pd
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8002))
+    app.run(host="0.0.0.0", port=port)
