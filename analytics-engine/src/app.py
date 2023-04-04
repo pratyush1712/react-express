da19905306ea0bb71e41580973ac01d36181e032
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-04-04 06:41:37 -0400

    fixing

--- analytics-engine/src/app.py
+++ analytics-engine/src/app.py
@@ -3,11 +3,17 @@ import pandas as pd
 import numpy as np
 import os
 
+def success_response(body):
+    return jsonify({"status": "success", "body": body})
+
+def error_response(message):
+    return jsonify({"status": "error", "message": message})
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return success_response("Hello World")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8002))
