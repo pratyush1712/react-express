c9bf629923a35bd59d82bd2c223f1791a25f834c
Pratyush Sudhakar <ps2245@cornell.edu>
2023-04-08 22:36:42 -0400

    add dev

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,5 +6,5 @@ dotenv.config({ path: path.join(__dirname, "../.env") });
 export const spotify = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  REDIRECT_URI: "http://localhost:8000/" || process.env.REDIRECT_URI,
+  REDIRECT_URI: process.env.REDIRECT_URI,
 };
